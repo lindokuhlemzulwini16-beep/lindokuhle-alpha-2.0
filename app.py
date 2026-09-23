a25895b6e388c1f7da629f21aa3583725461de14
lindokuhlemzulwini16-beep <lindokuhlemzulwini16@gmail.com>
2026-09-23 15:08:04 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="ALPHA 2.0 PRO")
+st.title("LINDOKUHLE ALPHA 2.0 PRO MAX - LIVE")
+st.success("No Excess - Max 2 Trades Protection ON")
+if st.button("GET SIGNAL"):
+    st.balloons()
+    st.write("BUY GOLD @ 2650")
+st.write("Trades: 0/2 today")
